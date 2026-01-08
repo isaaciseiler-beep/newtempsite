eb42d2ef964b64ac97915478711feed1035f8282
isaac seiler <isaaciseiler@gmail.com>
2026-01-08 12:58:59 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-41d52824b0bb4f44898c39e1c3c63cb8.r2.dev",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
